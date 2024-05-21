4f947b3299453da013a83ec13917504d12d53335
RamukRavindra <ravindrakumar3156@gmail.com>
2024-05-21 06:48:05 +0530

    Added Bootstrap extensions and started building project files

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Portfolio</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
